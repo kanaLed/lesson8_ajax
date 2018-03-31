35beee667d20e275bab1c3c14785e36d788c65e3
kana <kanaledbetter@comcast.net>
2018-03-31 19:16:36 +0000

    Lesson 8 final commit

--- Movie.js
+++ Movie.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
             $.ajax({
                 //Insert YOUR API Key after the '=' inside the single quotes.
-                url:'https://api.themoviedb.org/3/search/movie?api_key=INSERT-HERE',
+                url:'https://api.themoviedb.org/3/search/movie?api_key=8b0b4b5a6edae275f83cc9630b15cd94',
                 data: query
             })
             
@@ -57,7 +57,7 @@ $(document).ready(function(){
                  
                  *********************************Insert Code Below********************************************/                
                 
-                
+                $("#overview").html(json.results[0].overview);
                 
 
                 /***********************Retrieve Movie Release Date from Server & Add to HTML Document***********
@@ -70,7 +70,8 @@ $(document).ready(function(){
                  2.  Use as the DOM name where the id is "#release_date"
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
-                 *********************************Insert Code Below********************************************/               
+                 *********************************Insert Code Below********************************************/       
+                 $("#release_date").html(json.results[0].release_date);
 
 
 
@@ -86,6 +87,7 @@ $(document).ready(function(){
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
                  *********************************Insert Code Below********************************************/   
+                 $("#popularity").html(json.results[0].popularity);
 
   
 
@@ -100,7 +102,8 @@ $(document).ready(function(){
                  2.  Use as the DOM name where the id is "#original_language"
                  3.  Use the example above where the Movie Title was retrieved to complete this step!
                  
-                 *********************************Insert Code Below********************************************/                 
+                 *********************************Insert Code Below********************************************/      
+                 $("#original_language").html(json.results[0].original_language);
 
 
 
--- README.md
+++ README.md
@@ -4,19 +4,18 @@
 LIS 2360:  Web Application Development
 
 ### Assignment Date:  
-(Month Day, Year)
+03, 31, 2018
 
 ### Student Name:  
-(First and Last Name)
+Kana Ledbetter
 
 ### Project Description:
-(In your own words, briefly describe the assignment.)
+This assignment is to complete the Seminole Movie Connection Application.
 
 ### View Project:
-(Replace this statement with your Github Page URL that was created when you 
- published the project.)
+https://kanaled.github.io/lesson8_ajax/
 
 ### Lessons Learned in the Assignment:
-1. (Briefly describe a lesson/concept learned in this lesson.)
-2. (Briefly describe a lesson/concept learned in this lesson.)
-3. (Briefly describe a lesson/concept learned in this lesson.)
+1. I learned the introduction of AJAX.
+2. I learned what is JSON and how it works to update a webpage.
+3. I learned what an XMLHttpRequest Object.
